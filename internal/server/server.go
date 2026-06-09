eda2f6e0376af86779b6ca3b7d03b63051f9307f
elgeorgiou <1Eleanaki@gmail.com>
2026-06-09 13:14:50 +0300

    feat(ui): implement dark theme layout

--- internal/server/server.go
+++ internal/server/server.go
@@ -11,5 +11,13 @@ func Start() error {
 	http.HandleFunc("/", handlers.HomeHandler)
 	http.HandleFunc("/ascii-art", handlers.AsciiArtHandler)
 
+	http.Handle(
+		"/static/",
+		http.StripPrefix(
+			"/static/",
+			http.FileServer(http.Dir("static")),
+		),
+	)
+
 	return http.ListenAndServe(":8080", nil)
 }
